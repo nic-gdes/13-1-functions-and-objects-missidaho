4989b60f9550febc7f8ce1d2f3bc3926c1734529
Madeleine <mgleahy@nic.edu>
2023-11-14 15:11:04 -0800

    added Psuedo code

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 // Nothing here yet...
 // Make sure to link your JS in index.html!
+
+// Create a Car class, with a constructor that accepts 3 parameters: make (ex: Chevy, Ford, Toyota), model (ex: Camero, F150, Rav4), and year.
+
+
+// Car should also include a method, .describe which simply returns "This is a YEAR MODEL, made by MAKE." replacing YEAR, MODEL and MAKE with the correct information from the Car object.
+// const chevyTahoe = new Car("Chevrolet", "Tahoe", "2017");
+// chevyTahoe.describe // This is a 2017 Tahoe, made by Chevrolet.
+// Create 4 cars (with any make, model and year) and push them all to a new array called allCars.
+// Create a new array called allCars.
+// You will need to create 4 new variables, using the const {{carname}} = new Car() syntax. {{ carname }} is just whatever variable name you'll choose.
+// Write a function with a single parameter car that, when called, will console.log the Car.describe method for the car used in the parameter.
+// Declare the function using function {{ functionName }} (car) {};
+// Inside the function's code block, simply return car.describe; so the function returns the describe phrase for the car in the parameters.
+// Create an array called allCars, that includes all of the created cars. You can use array.push() or simply create the array with the data.
+// Loop through allCars and call the function to log each car's describe method to the console one by one.
+// You can use a for(), or forEach loop to achieve this. Remember to use allCars.length in your end condition if you use a for() loop.
+// Remember you can call a function by simply writing the function with parenthesis around the parameters:
+
